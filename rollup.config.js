e29d1fa105d68e6c196a828d5f88bbe3943059b3
Andrea Carraro <me@andreacarraro.it>
2018-07-03 18:58:41 +0200

    Configure rollup.js to build es, cjs and umd modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,51 @@
-import flow from 'rollup-plugin-flow';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 
+let pkg = require('./package.json');
+let external = []
+  // Mark dependencies and peerDependencies as external
+  .concat(
+    Object.keys(pkg.dependencies), Object.keys(pkg.peerDependencies)
+  );
+
 export default {
   input: 'src/index.js',
+  external: external,
   output: [
     {
-      file: './dist/index.js',
-      format: 'cjs'
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named',
     },
     {
-      file: './dist/umd/index.js',
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+      exports: 'named',
+    },
+    {
+      file: pkg.browser,
       format: 'umd',
-      name: 'Clickable'
-    }
+      sourcemap: true,
+      exports: 'named',
+      name: 'ReactClickable',
+      globals: {
+        react: 'React',
+      },
+    },
   ],
   plugins: [
-    flow(),
+    resolve(),
+    babel({
+      exclude: 'node_modules/**',
+      plugins: ['external-helpers'],
+    }),
     commonjs({
-      include: ['node_modules/**'],
-      exclude: ['node_modules/process-es6/**'],
       namedExports: {
-        'node_modules/react/index.js': ['Component', 'createElement'],
-        'node_modules/react-dom/index.js': ['render']
+        'node_modules/react/index.js': ['Component'],
       }
     }),
-    resolve({
-      module: true,
-      main: true
-    }),
-    babel({ exclude: 'node_modules/**' })
   ]
 };
